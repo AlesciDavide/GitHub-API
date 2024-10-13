156651cd88e68deb6946064a3e76601fe6e85e59
AlesciDavide <davi.alesci@gmail.com>
2024-10-13 20:18:43 +0200

    ricerca per nome repo

---
+++ src/components/homepage/SingleProject.vue
@@ -0,0 +1,42 @@
+<script>
+import { store } from '../../store.js';
+import axios from 'axios';
+
+export default {
+    data() {
+        return {
+            store,
+        }
+    },
+    
+}
+</script>
+
+<template>
+
+    <section v-for="project in store.project" :key="project.id">
+        <h1>{{ project.name }}</h1>
+
+        <button>
+            <a :href="project.html_url" target="_blank">Vai alla repo</a>
+        </button>
+
+    </section>
+</template>
+
+<style lang="scss" scoped>
+section {
+    width: calc((80vw / 4) - 50px);
+    height: 200px;
+    margin: 20px 5px;
+    background-color: rgb(106, 106, 255);
+    border-radius: 20px;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+
+    h1 {
+        margin-top: 5px;
+    }
+}
+</style>
--- src/components/pages/MainProject.vue
+++ src/components/pages/MainProject.vue
@@ -1,23 +1,124 @@
 <script>
-import {store} from '../../store.js';
+import { store } from '../../store.js';
+import axios from 'axios';
+import SingleProject from '../homepage/SingleProject.vue';
 
-export default{
+export default {
     data() {
-        return{
+        return {
             store,
-            
         }
+    },
+    components: {
+        SingleProject,
+    },
+    methods: {
+        Projects() {
+
+            if(this.store.search.length > 0){
+
+                axios.get(`https://api.github.com/search/repositories?q=${this.store.search}`)
+                    .then((response) => {
+                        console.log(response.data.items);
+                        this.store.project = (response.data.items);
+                    })
+                    .catch((err) => {
+                        console.error('Errore nella richiesta:', err);
+                    });
+            }
+
+
+        }
+    },
+    mounted() {
+        this.Projects();
     }
 }
 </script>
 
 <template>
-    <h1>
-        main project
-    </h1>
+    <div>
+        <input type="text" placeholder="Cerca una repo" v-model="store.search" @keyup.enter="Projects">
+        <button @click="Projects"><i class="fa fa-search"></i></button>
+
+    </div>
+    <article v-if="store.search.length > 0" >
+        <SingleProject />
+
+    </article>
 </template>
 
 <style lang="scss" scoped>
+article {
+    width: 80vw;
+    height: 100%;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-wrap: wrap;
 
+    section {
+        width: calc(80vw / 5);
+        gap: 20px;
+        background-color: aqua;
+    }
+}
 
+div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    input {
+        width: 100%;
+        max-width: 220px;
+        height: 45px;
+        padding: 12px;
+        border-radius: 12px;
+        border: 1.5px solid lightgrey;
+        outline: none;
+        transition: all 0.3s cubic-bezier(0.19, 1, 0.22, 1);
+        box-shadow: 0px 0px 20px -18px;
+    }
+
+    input:hover {
+        border: 2px solid lightgrey;
+        box-shadow: 0px 0px 20px -17px;
+    }
+
+    input:active {
+        transform: scale(0.95);
+    }
+
+    input:focus {
+        border: 2px solid grey;
+    }
+
+    /* bottone */
+    button {
+        width: 40px;
+        max-width: 220px;
+        height: 40px;
+        padding: 12px;
+        border-radius: 12px;
+        border: 1.5px solid lightgrey;
+        outline: none;
+        transition: all 0.3s cubic-bezier(0.19, 1, 0.22, 1);
+        box-shadow: 0px 0px 20px -18px;
+    }
+
+    button:hover {
+        border: 2px solid lightgrey;
+        box-shadow: 0px 0px 20px -17px;
+    }
+
+    button:active {
+        transform: scale(0.95);
+    }
+
+    button:focus {
+        border: 2px solid grey;
+    }
+}
 </style>
